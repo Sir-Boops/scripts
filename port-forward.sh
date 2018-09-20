18a426e979ecbbb3cc969d34e49334fa66073dfc
Sir_Boops <admin@boops.me>
2018-09-20 08:50:55 -0600

    To dst

--- port-forward.sh
+++ port-forward.sh
@@ -6,6 +6,11 @@ echo ""
 read -p "IP address: " IP_ADDRESS
 clear
 
+# Get public IP
+echo "What is your public IP?"
+echo ""
+read -p "Public IP: " PUBLIC_IP
+
 # Get TCP ports to forward
 echo "What TCP ports do you wish to forward"
 echo "Example 80 53 500"
@@ -20,10 +25,10 @@ clear
 
 # Loop over and forward all the tcp ports
 for PORT in $TCP_PORTS; do
-  iptables -t nat -A PREROUTING -p tcp --dport $PORT -j DNAT --to-destination $IP_ADDRESS:$PORT
+  iptables -t nat -A PREROUTING -p tcp --dport $PORT -j DNAT --destination $PUBLIC_IP --to-destination $IP_ADDRESS:$PORT
 done
 
 # Loop over and forward all the UDP ports
 for PORT in $UDP_PORTS; do
-  iptables -t nat -A PREROUTING -p udp --dport $PORT -j DNAT --to-destination $IP_ADDRESS:$PORT
+  iptables -t nat -A PREROUTING -p udp --dport $PORT -j DNAT --destination $PUBLIC_IP --to-destination $IP_ADDRESS:$PORT
 done
