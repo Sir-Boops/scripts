bb9918c83e8d5d17f928923ab614450f1b7e87c8
Sir-Boops <admin@boops.me>
2023-02-14 19:49:19 -0700

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -38,3 +38,7 @@ echo "To start upnp: miniupnpd -d -1 -i ens3 -a wg0 -A \"allow 1024-65535 192.16
 
 # Finally start Wireguard
 ip link set up dev wg0
+
+
+
+echo "Paste public key now"; read publicKey; ip link; echo "What interface are you using?"; read interFace; apt install wireguard -y; ip link add dev wg0 type wireguard; ip address add dev wg0 192.168.24.1/24; wg genkey > priv.key; wg pubkey < priv.key > pub.key; wg set wg0 listen-port 25565; wg set wg0 private-key priv.key; wg set wg0 peer `echo $publicKey` allowed-ips 0.0.0.0/0; wg showconf wg0 > wg.conf; ufw disable; sysctl -w net.ipv4.ip_forward=1; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o `echo $interFace` -j MASQUERADE; ip link set up dev wg0; echo ""; echo "Setup is complete this servers public key is:"`cat pub.key`
